75aab295b339b8dfbde4568e455b511c9003c81c
clddup <105873086+clddup@users.noreply.github.com>
2024-12-07 04:19:33 +0000

    feat: bug复现

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {ref} from 'vue'
+import { AutoComplete, Input } from 'ant-design-vue'
+
+const keyword = ref('');
+const options = ref([]);
+function handleSearch(value){
+  options.value = !value
+    ? []
+    : [{ value }, { value: value + value }, { value: value + value + value }];
+}
+
+function onSelect(value){
+  console.log('onSelect', value);
+  options.value = []
+}
+</script>
+
+<template>
+  <main>
+    <div>
+      搜索值： {{ keyword }}
+    </div>
+    <br>
+    <br>
+    <AutoComplete 
+      v-model:value="keyword"
+      :options="options"
+      @search="handleSearch"
+      @select="onSelect"
+    >
+      <Input placeholder="请输入" />
+    </AutoComplete>
+  </main>
+</template>
+
+<style scoped>
+main{
+  width: 300px;
+  margin: 20px auto;
+}
+</style>
